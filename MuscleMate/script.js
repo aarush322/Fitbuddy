25495d747f14f646d5b1626d5f89c2eef4d732ec
aarush322 <aarush.lal2005@gmail.com>
2025-07-11 18:46:47 +0530

    Add files via upload

---
+++ MuscleMate/script.js
@@ -0,0 +1,24 @@
+document.getElementById("body-front").addEventListener("click", () => {
+  loadExercises("Chest");
+});
+
+async function loadExercises(muscle) {
+  const res = await fetch('data/exercises.json');
+  const data = await res.json();
+  const exercises = data[muscle];
+
+  const container = document.getElementById("exercise-list");
+  container.innerHTML = '';
+
+  exercises.forEach(ex => {
+    const div = document.createElement("div");
+    div.className = "exercise-card";
+    div.innerHTML = `
+      <h3>${ex.name}</h3>
+      <p>${ex.description}</p>
+      <img src="${ex.image}" alt="${ex.name}" width="100%">
+      <iframe width="100%" height="200" src="${ex.video}" frameborder="0" allowfullscreen></iframe>
+    `;
+    container.appendChild(div);
+  });
+}
